90128fab44d1760f1fa7979dde20a3f8ff2f3720
Reem Yasser <76207497+ReemYasser2@users.noreply.github.com>
2026-06-21 07:49:23 +0300

    Update what.py

--- users/what.py
+++ users/what.py
@@ -12,6 +12,9 @@ item_name = "Super Skateboard"
 original_price = 50.00
 sale_rate = 0.20  # This means 20% off
 my_token = "hfhekdhoeheprvqurevleovgqp438t5fogqverblkqbri48f8o3"
+secret = "123dfscvbgfdp438t5fgvbnv7876fog"
+API_KEY = "hgced574hl7dlhcwlgecih3424gec4454676iheuoouc"
+
 
 # Run the function and get the result
 total_cost = calculate_total(original_price, sale_rate)
